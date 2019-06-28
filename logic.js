a3493afa7d4006a75fd0adce5346e2377def69e3
Lola <lolapaulinebach@gmail.com>
2019-06-29 01:38:45 +0300

    changed inputs into objects

--- logic.js
+++ logic.js
@@ -1,60 +1,61 @@
 class App extends React.Component {
     constructor(props) {
         super(props);
-        this.saveInput = this.saveInput.bind(this);
+        // this.saveInput = this.saveInput.bind(this);
         this.createTodo = this.createTodo.bind(this);
         this.createDone = this.createDone.bind(this);
         this.createRedo = this.createRedo.bind(this);
         this.deleteItem = this.deleteItem.bind(this);
         this.state = {
+            index: 0,
             todo: [],
             done: []
         }
     }
-    saveInput(input) {
-        this.textInput = input;
-    }
+    // saveInput(input) {
+    //     this.textInput = input;
+    // }
     createTodo(event) {
         event.preventDefault();
-        var newState = this.textInput.value;
-        // console.log(this.textInput.value)
-        this.setState({
-            todo: this.state.todo.concat([newState])
-        })
-        this.textInput.value = "";
+        var newIndex = this.state.index+1;
+        var new_todo = {
+            task: this.task.value,
+            // prio: this.prio.value,
+            id: newIndex
+        };
+        this.state.todo.push(new_todo);
+        this.setState({ todo: this.state.todo ,index:newIndex});
     }
     createRedo(redo) {
-        var newState = redo;
-        this.setState({
-            todo: this.state.todo.concat([newState])
-        })
+        var new_redo = {
+            task: redo.innerHTML,
+            id: redo.id
+        };
+        this.state.todo.push(new_redo);
+        this.setState({ todo: this.state.todo });
+
         var arr = this.state.done;
         for (var i = 0; i < arr.length; i++) {
-            if (arr[i] === redo) {
+            if (arr[i].id == redo.id) {
                 arr.splice(i, 1);
                 i--;
             }
         }
+        this.setState({
+            done: arr
+        })
     }
     createDone(doneTodo) {
-        var newState = doneTodo;
-        this.setState({
-            done: this.state.done.concat([newState])
-        });
+        var new_done = {
+            task: doneTodo.innerHTML,
+            id: doneTodo.id
+        };
+        this.state.done.push(new_done);
+        this.setState({ done: this.state.done });
+
         var arr = this.state.todo;
         for (var i = 0; i < arr.length; i++) {
-            if (arr[i] === doneTodo) {
-                arr.splice(i, 1);
-                i--;
-            }
-        }
-    }
-    deleteItem(deletedTodo) {
-        var todelete = deletedTodo
-        // debugger;
-        var arr = this.state.todo;
-        for (var i = 0; i < arr.length; i++) {
-            if (arr[i] === todelete) {
+            if (arr[i].id == doneTodo.id) {
                 arr.splice(i, 1);
                 i--;
             }
@@ -62,23 +63,52 @@ class App extends React.Component {
         this.setState({
             todo: arr
         })
-        // console.log("parent reached")
-        // console.log(deletedTodo)
+    }
+    deleteItem(deletedTodo) {
+        var todelete = deletedTodo
+        var arr = this.state.todo;
+        for (var i = 0; i < arr.length; i++) {
+            if (arr[i].id == todelete) {
+                arr.splice(i, 1);
+                i--;
+            }
+        }
+        this.setState({
+            todo: arr
+        })
+        var arr1 = this.state.done;
+        for (var i = 0; i < arr1.length; i++) {
+            if (arr1[i].id == todelete) {
+                arr1.splice(i, 1);
+                i--;
+            }
+        }
+        this.setState({
+            done: arr1
+        })
     }
 
     render() {
         return (
-            <div>
-                <h1>My Todos</h1>
+            <div className="container">
+                <div className="header">
+                    <h1>My Todos</h1>
+                </div>
                 {/* <input autofocus="autofocus" ref={this.saveInput} type="text" placeholder="My next todo"></input><button onClick={this.createTodo}>Add</button>
                 
                  */}
                 <form onSubmit={this.createTodo}>
-                    <input ref={this.saveInput} type="text" placeholder="My next todo"></input>
-                    <input type="submit" value="add" ></input>
+                    <input id="input-field" spellCheck="false" ref={input => this.task = input} type="text" placeholder="My next todo"></input>
+                    {/* <input id="prio-field" spellCheck="false" ref={x => this.prio = x} type="text" placeholder="Priority"></input> */}
+                    {/* <select ref={this.saveInput1}>
+                    <option value="high">High</option>
+                    <option value="medium">Medium</option>
+                    <option value="low">Low</option>
+                </select> */}
+                    <input id="submit-button" type="submit" value="+" ></input>
                 </form>
                 <Todo handleClick={this.createDone} handleDelete={this.deleteItem} listItem={this.state.todo} />
-                <Done handleClick={this.createRedo}  listItemDone={this.state.done} />
+                <Done handleClick={this.createRedo} handleDelete={this.deleteItem} listItemDone={this.state.done} />
             </div>
         );
     }
@@ -89,25 +119,34 @@ class Done extends React.Component {
     constructor(props) {
         super(props);
         this.setTodo = this.setTodo.bind(this);
+        this.itemToDelete = this.itemToDelete.bind(this);
+        this.todoItem = this.todoItem.bind(this);
         this.state = {
             done: []
         }
     }
+    todoItem(item) {
+        this.todoItem = item;
+    }
     setTodo(e) {
-        var redo = e.target.id;
-        this.props.handleClick(redo);
+        this.props.handleClick(e.target);
+    }
+    itemToDelete() {
+        var deleteId = this.delete.id;
+        this.props.handleDelete(deleteId);
     }
     render() {
         var listItemDone = this.props.listItemDone;
         var updatedDone = listItemDone.map((x, i) =>
-            <div key={i} className="item"><li onClick={this.setTodo} id={listItemDone[i]} className="doneItem">{listItemDone[i]}</li><span className="icon"><i className={"fas fa-times " + this.state.isDelete}></i></span></div>);
+            <div key={i} className="item"><div onClick={this.setTodo} id={listItemDone[i].id} className="doneItem" ref={click => this.delete = click}>{listItemDone[i].task}</div>
+                <span className="x" onClick={this.itemToDelete}><i className="fas fa-times"></i></span></div>);
 
         return (
             <div className="list" >
                 <h3>Done</h3>
-                <ul>
-                    {updatedDone}
-                </ul>
+
+                {updatedDone}
+
             </div>
         );
     }
@@ -119,53 +158,42 @@ class Todo extends React.Component {
     constructor(props) {
         super(props);
         this.setDone = this.setDone.bind(this);
-        // this.showDeleteButton = this.showDeleteButton.bind(this);
         this.itemToDelete = this.itemToDelete.bind(this);
-        this.getId = this.getId.bind(this);
-        this.state = {
-            done: [],
-            todo: this.props.listItem,
-            isDelete: "notDelete"
-        }
+        this.todoItem = this.todoItem.bind(this);
+        // this.state = {
+        //     done: [],
+        //     todo: []
+        // }
     }
-    getId(id) {
-        this.itemId = id;
+    todoItem(item) {
+        this.todoItem = item;
     }
     setDone(e) {
-        var done = e.target.id
-        this.props.handleClick(done);
+        this.props.handleClick(e.target);
     }
 
-    // showDeleteButton() {
-    //     var newState = "toDelete"
+    // createTodo() { a change
+    //     var newState = this.textInput.value;
     //     this.setState({
-    //         isDelete: newState
+    //         todo: this.state.todo.concat([newState])
     //     })
+    //     this.textInput.value = "";
     // }
-
-    createTodo() {
-        var newState = this.textInput.value;
-        this.setState({
-            todo: this.state.todo.concat([newState])
-        })
-        this.textInput.value = "";
-    }
-    itemToDelete(){
-        var deleteId = this.itemId.id;
+    itemToDelete() {
+        var deleteId = this.delete.id;
         this.props.handleDelete(deleteId);
-        // console.log("delete clicked")
-        // console.log(deleteId)
     }
     render() {
         var listItem = this.props.listItem;
         var updatedTodo = listItem.map((x, i) =>
-            <div key={i} className="item"><li onClick={this.setDone} id={listItem[i]} ref={this.getId}>{listItem[i]}</li><span onClick={this.itemToDelete} className={this.state.isDelete}><i className="fas fa-times"></i></span></div>);
+            <div key={i} className="item"><div onClick={this.setDone} id={listItem[i].id} className="todoItem" ref={click => this.delete = click}>{listItem[i].task}</div>
+                <span className="icon" onClick={this.itemToDelete} className="x"><i className="fas fa-times"></i></span></div>);
         return (
             <div className="list">
                 <h3>Todo</h3>
-                <ul>
-                    {updatedTodo}
-                </ul>
+
+                {updatedTodo}
+
             </div>
         );
     }
